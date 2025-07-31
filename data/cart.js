e914854241ed920c1b1f914d7265207e4c20e51a
priyanshu pundir <priyanshupundir345@gmail.com>
2025-07-31 09:03:00 +0530

    save cart in localstorage

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,7 @@
- export let cart = [{
+ export let cart = JSON.parse(localStorage.getItem('cart'));
+ 
+ if(!cart){
+    cart =  [{
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity: 2,
 
@@ -6,7 +9,11 @@
     productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
     quantity: 1
  }];
+ }
  
+ function saveTOStorage(){
+    localStorage.setItem('cart', JSON.stringify(cart));
+ }
  
  export function addToCart(productId){
    let matchingItem;
@@ -23,11 +30,12 @@
                     quantity: 1
                 });
             }
+    saveTOStorage();
  }
 
  export function removeFromCart(productId) {
     const newCart = [];
-    
+
     cart.forEach((cartItem) => {
         if(cartItem.productId !== productId){
             newCart.push(cartItem);
@@ -36,4 +44,5 @@
 
 
     cart = newCart;
+    saveTOStorage();
  }
